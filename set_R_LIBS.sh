973c9902db76e46ebef2f3b5a324daa1180aca4b
Scott Givan <sgivan@yahoo.com>
2023-04-11 16:10:22 -0400

    Added -m option to specify the "new" major release version. Default is 5.

--- set_R_LIBS.sh
+++ set_R_LIBS.sh
@@ -2,9 +2,11 @@
 
 #TEMP=`getopt -o he:E: -- "$@"`
 #TEMP=`getopt -o v -- "$@"`
-TEMP=$(getopt -o v -- "$@")
+TEMP=$(getopt -o vm: -- "$@")
 
 verbose=0
+
+majorversion=5
 
 krelease=$(uname -r)
 
@@ -14,7 +16,7 @@ eval set -- "$TEMP"
 while true ; do
     case "$1" in
         -v) verbose=1 ; shift ;;
-#        -e) exp1=$2 ; shift 2 ;;
+        -m) majorversion=$2 ; shift 2 ;;
 #        -E) exp2=$2 ; shift 2 ;;
         --) shift ; break ;;
         *) break ;;
@@ -22,17 +24,16 @@ while true ; do
 done
 
 case "$majorK" in
-    "3") 
+    $majorversion)
+        if [[ $verbose == 1 ]]; then
+            echo "setting R_LIBS_USER for new cluster";
+        fi
+        export R_LIBS_USER="$HOME/R/x86_64-pc-linux-gnu-library/4.2-new" ;; 
+    *)
         if [[ $verbose == 1 ]]; then 
             echo "setting R_LIBS_USER for old cluster";
         fi
         export R_LIBS_USER="$HOME/R/x86_64-pc-linux-gnu-library/4.2-old"
         ;; 
-    "5")
-        if [[ $verbose == 1 ]]; then
-            echo "setting R_LIBS_USER for new cluster";
-        fi
-        export R_LIBS_USER="$HOME/R/x86_64-pc-linux-gnu-library/4.2-new" ;; 
-    *) break ;;
 esac
 
